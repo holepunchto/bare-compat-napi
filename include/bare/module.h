bd620a3f344778a272e3a260d89ee577b68b2b9a
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-01-10 16:26:05 +0100

    Fix `BARE_MODULE_VERSION`

--- include/bare/module.h
+++ include/bare/module.h
@@ -6,7 +6,7 @@
 #include <node_api.h>
 #include <stddef.h>
 
-#define BARE_MODULE_VERSION 1
+#define BARE_MODULE_VERSION 0
 
 #define BARE_MODULE(id, fn) \
   napi_value napi_register_module_v1(napi_env env, napi_value exports) { \
@@ -17,16 +17,21 @@ typedef struct bare_module_s bare_module_t;
 
 typedef js_value_t *(*bare_module_cb)(js_env_t *env, js_value_t *exports);
 
+/** @version 0 */
 struct bare_module_s {
   int version;
+
+  /** @since 0 */
   const char *filename;
+
+  /** @since 0 */
   bare_module_cb init;
 };
 
 static inline void
-bare_module_register (bare_module_t *mod) {
+bare_module_register(bare_module_t *mod) {
   napi_module napi_mod = {
-    mod->version,
+    NAPI_MODULE_VERSION,
     0,
     mod->filename,
     mod->init,
