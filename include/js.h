70d503ece3da4c4b22e9ebc6cf0c737ecb1b144b
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-04-11 09:36:11 +0200

    Remove `le` postfix

--- include/js.h
+++ include/js.h
@@ -612,7 +612,7 @@ js_create_external_string_utf16le(js_env_t *env, utf16_t *str, size_t len, js_fi
 #else
   if (copied) *copied = true;
 
-  napi_status status = napi_create_string_utf16le(env, str, len, result);
+  napi_status status = napi_create_string_utf16(env, str, len, result);
 
   if (status == napi_ok && finalize_cb) finalize_cb(env, str, finalize_hint);
 #endif
